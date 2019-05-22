f045f4351f6ab17a285fefbfdb256b700458743b
Owen Carter <o.carter@activevideo.com>
2019-05-22 21:06:48 +0200

    Template for wifi settings file

---
+++ mywifi.sample.h
@@ -0,0 +1,14 @@
+// Wifi Network Definitions:
+// Rename this file to mywifi.h to enable and use.
+// Because mywifi.h is in the .gitignore file you can avoid 
+//  accidentally leaking wifi details by doing this.
+
+// A more secure accesspoint config
+//#define ACCESSPOINT true
+//const char* ssid = "my-demo";
+//const char* password = "my-password"; 
+
+// Conect to a network
+const char* ssid = "my-wifi";
+const char* password = "my-password"; 
+
